ff57a4c5420b7e9757d4a142538075879711d236
Eric20Junior <eric20junior@gmail.com>
2021-08-26 07:42:39 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,7 @@
  * Return: Always 0 (Sucess)
  * */
 int main()
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
 {
-	/*Sizes of datatype*/
 	printf("size of int:%zu bytes\n", sizeof(intType));
 	printf("size of int:%zu bytes\n", sizeof(floatType));
 	printf("size of int:%zu bytes\n", sizeof(doubleType));
