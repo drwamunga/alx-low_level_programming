b661a4b4406ce0d807021888c60aaa7e9c1ca2aa
Eric20Junior <eric20junior@gmail.com>
2021-08-31 01:49:45 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * write a program tha prints Holberton, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Holberton\n");
+return (0);
+}
