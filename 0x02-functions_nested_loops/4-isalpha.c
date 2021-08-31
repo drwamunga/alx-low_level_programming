e4dac4a37013eaaceedfae9ba169cf8be7e1a0e4
Eric20Junior <eric20junior@gmail.com>
2021-08-31 08:35:04 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_isalpha - write a function that checks for alphabet character
+*
+*Return: 1 if c is a letter, lowercase or uppercase, otherwise 0
+*/
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <=122) || (c >= 65 && <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
