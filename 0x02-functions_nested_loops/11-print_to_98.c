beade6aa99bfad3ec3837cd983978aa77ac19766
Eric20Junior <eric20junior@gmail.com>
2021-09-01 14:02:56 -0700

    times_table

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,33 +8,13 @@
 */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n < 98)
 	{
-		while (n >= 98)
-		{
+		for (n = 0; n <= 97; n++)
 			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
-	}
-	else if (n >= 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
 	}
 	else
-	{
-		printf("98");
-	}
-	printf("\n");
+		for (n = 0; n > 98; n--)
+			printf("%d", n);
+	printf("98\n");
 }
