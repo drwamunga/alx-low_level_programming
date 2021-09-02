9e03ff614c723cbb96aa0c16485e4fa8dba9abcd
Eric20Junior <eric20junior@gmail.com>
2021-09-02 05:42:34 -0700

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isdigit - checks for digits
+*@c: checking using c
+*Return: 1 is a digit, otherwise 0
+*/
+int _isdigit(int c)
+{
+	for (c = 0; c <= 9; c++)
+		return (1);
+	else
+		return (0);
+}
