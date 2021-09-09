9001270adff6eae50e3962398154b101e6988770
Eric20Junior <eric20junior@gmail.com>
2021-09-09 03:04:18 -0700

    task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strcat - concatenate two strings
  * @dest: char string to concatenate to
@@ -8,17 +9,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i, c;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (j = 0; src[j] != '\0'; j++)
-		;
-	dest[i] = src[j];
-	i++;
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[i] = src[c];
+		i++;
+	}
 
 	dest[i] = '\0';
-
 	return (dest);
 }
