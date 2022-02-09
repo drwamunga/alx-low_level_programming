c4c80980fd52deaf985a61b2adcac9b345fbb2f7
Eric20Junior <eric20junior@gmail.com>
2022-02-09 14:33:55 -0800

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
-"""
-Module Island Perimeter
+"""Contains function that returns the perimeter of the
+island described in grid
 """
 
+
+def determine_soroundings(array, y, x):
+    """Determines whether soroundings has water or not
+    also if it's next or previous is at the ends
+    Args:
+        array (list): grid
+        y (int): current list index in `array`
+        x (int): current index in the list in `array`
+    """
+    mask = 1
+    top = array[y - 1][x] ^ mask if y > 0 else 1
+    bottom = array[y + 1][x] ^ mask if y < (len(array) - 1) else 1
+    left = array[y][x - 1] ^ mask if x > 0 else 1
+    right = array[y][x + 1] ^ mask if x < (len(array[y]) - 1) else 1
+    positions = top + bottom + right + left
+    return positions
+
+
 def island_perimeter(grid):
-"""create a function thats returns the perimeter of the island descripted in grid"""
-    i = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                elif row == 0 or grid[row - 1][col] == 0:
-                    i += 1
-                elif row == (len(grid) - 1) or grid[row + 1][col] == 0:
-                    i += 1
-                elif col == 0 or grid[row][col - 1] == 0:
-                    i += 1
-                elif col == (len(grid[0] - 1) or grid[row][col + 1] == 0:
-                    i += 1
-    return i
+    """Gets the perimeter determined by sorounding.
+    all sides that do not have water we increase perimeter.
+    Args:
+        grid (list): list of lists
+    """
+    land = 1
+    perimeter = 0
+    for i in range(len(grid)):
+        for j, x in enumerate(grid[i]):
+            if x == land:
+                perimeter += determine_soroundings(grid, i, j)
+    return perimeter
